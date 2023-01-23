ca3ec3264615554978cc5c13b881f06f1c6f3988
Kashinath11 <bken25gourkhali@gmail.com>
2023-01-23 22:36:46 +0545

    first_commit

---
+++ user/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+# Create your views here.
+
+def myfunctioncall(request):
+    return render(request, 'index.html')
+
+def myfunctionabout(request):
+    return HttpResponse("About section")
+
+def intro(request, name, age):
+    mydictionary ={
+        "name":name,
+        "age":age
+    }
+    return JsonResponse(mydictionary)
+
+def mycommonpage(request):
+    return render(request, 'common.html')
